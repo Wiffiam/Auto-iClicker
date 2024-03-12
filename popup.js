be9d958e31916cfef9c00cd0958b0977169a3d7a
Wiffiam <72940715+Wiffiam@users.noreply.github.com>
2024-03-12 13:53:32 -0400

    Improved next answer

--- popup.js
+++ popup.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     orderInputSection.style.display = 'none';
 
-    displayNextAnswers();
+    setTimeout(function() {
+        displayNextAnswers();
+    }, 500);
 
     chrome.storage.local.get(['multipleChoiceOrder', 'numericOrder', 'shortAnswerOrder', 'selectAllOrder', 'orderInputSectionVisible'], function(result) {
         if (result.orderInputSectionVisible) {
@@ -59,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.storage.local.set({ 'power': powerState }, function() {
             console.log(`Extension ${powerState}`);
             powerButtonOn.style.display = powerSwitch.checked ? 'block' : 'none';
+            displayNextAnswers();
             powerButtonOff.style.display = powerSwitch.checked ? 'none' : 'block';
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {command: powerSwitch.checked ? "start" : "stop"});
